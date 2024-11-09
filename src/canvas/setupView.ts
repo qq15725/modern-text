8a099c86dc404e09ed30c4e238d4b46ad077b466
wengxiangmin <157215725@qq.com>
2024-11-09 18:26:49 +0800

    feat: change canvas render viewBox

--- src/canvas/setupView.ts
+++ src/canvas/setupView.ts
@@ -5,13 +5,12 @@ export function setupView(ctx: CanvasRenderingContext2D, pixelRatio: number, bou
   const view = ctx.canvas
   view.dataset.viewbox = String(`${left} ${top} ${width} ${height}`)
   view.dataset.pixelRatio = String(pixelRatio)
-  view.width = Math.max(1, Math.ceil(width * pixelRatio))
-  view.height = Math.max(1, Math.ceil(height * pixelRatio))
-  view.style.marginTop = `${top}px`
-  view.style.marginLeft = `${left}px`
-  view.style.width = `${width}px`
-  view.style.height = `${height}px`
+  const canvasWidth = width + left
+  const canvasHeight = height + top
+  view.width = Math.max(1, Math.ceil(canvasWidth * pixelRatio))
+  view.height = Math.max(1, Math.ceil(canvasHeight * pixelRatio))
+  view.style.width = `${canvasWidth}px`
+  view.style.height = `${canvasHeight}px`
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
   ctx.scale(pixelRatio, pixelRatio)
-  ctx.translate(-left, -top)
 }
