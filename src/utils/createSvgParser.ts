caa3156a51b63662302eb3de9bee8cbc8ec28638
wxm <157215725@qq.com>
2026-01-04 19:14:06 +0800

    fix: SvgParser

--- src/utils/createSvgParser.ts
+++ src/utils/createSvgParser.ts
@@ -1,6 +1,5 @@
-import type { Path2DSet } from 'modern-path2d'
 import type { SvgLoader } from './createSvgLoader'
-import { svgToDom, svgToPath2DSet } from 'modern-path2d'
+import { Path2DSet, svgToDom, svgToPath2DSet } from 'modern-path2d'
 
 export interface SvgParser {
   parsed: Map<string, { dom: SVGElement, pathSet: Path2DSet }>
@@ -13,14 +12,22 @@ export function createSvgParser(loader: SvgLoader): SvgParser {
   function parse(svg: string): { dom: SVGElement, pathSet: Path2DSet } {
     let result = parsed.get(svg)
     if (!result) {
-      const dom = svgToDom(
-        loader.needsLoad(svg)
-          ? loader.loaded.get(svg) ?? '<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" viewBox="0 0 0 0" />'
-          : svg,
-      )
-      const pathSet = svgToPath2DSet(dom)
-      result = { dom, pathSet }
-      parsed.set(svg, result)
+      const svgString = loader.needsLoad(svg)
+        ? loader.loaded.get(svg)
+        : svg
+      if (svgString) {
+        const dom = svgToDom(svgString)
+        const pathSet = svgToPath2DSet(dom)
+        result = { dom, pathSet }
+        parsed.set(svg, result)
+      }
+      else {
+        const dom = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        dom.setAttribute('width', '0')
+        dom.setAttribute('height', '0')
+        dom.setAttribute('viewBox', '0 0 0 0')
+        result = { dom, pathSet: new Path2DSet() }
+      }
     }
     return result
   }
